9aeb3eb2d2c18e92f26ac06702e926954512eb8e
Abhay Rane <raneabhay63@gmail.com>
2025-09-30 15:52:05 +0530

    day7
    workshop

--- script.js
+++ script.js
@@ -4,3 +4,48 @@ result=a+b;
 sub=a-b;
 console.log(result)
 console.log(sub)
+
+let car="volvo",
+year=2025,
+type=null
+console.log(car,year,type)
+console.log(typeof(car))
+console.log(typeof(year))
+console.log(typeof(type))
+
+let c="Harry"
+let v="10"
+console.log(typeof(c+v))
+
+let fruits=["mango","apple","orange"];
+//console.log(fruits)
+//console.log("Total fruits :",fruits.length)
+//console.log(fruits.[0])
+fruits.push("grapes");
+console.log(fruits)
+
+// let student={
+//     name: "sejal",
+//     age: "19",
+//     course: "Computer science",
+//     isEnrolled: true
+// }
+// console.log(student)
+// console.log(student.name)
+
+let x=2;
+console.log("addition : x + 3 =",x + 3)
+console.log("subtraction : x - 3 =",x - 3)
+console.log("Multiplication : x * 3 =",x * 3)
+console.log("Poewer : x ** 3 =",x ** 3)
+console.log("incr : x ++  =", ++x)
+console.log("decr : x --  =", --x)
+console.log("Division : x / 3 =",x / 3)
+console.log("Rem : x % 3 =",x % 3)
+
+let y=2;
+y++
+z=y
+console.log(z)
+
+
